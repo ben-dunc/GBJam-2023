27aa9014ec567fa6980a98a66b4b85db77346d14
Ben Duncan <58539746+ben-dunc@users.noreply.github.com>
2023-09-21 23:17:16 -0600

    Player animations! and pixel perfect camera.

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour {
+
+    Rigidbody2D rb2;    
+    Animator anim;
+    string dir = "s";
+    readonly float MOVEMENT_DEADZONE = 0.01f;
+    
+    void Start() {
+        rb2 = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() {
+        string clipToPlay = GetAnimationName();
+        anim.Play(clipToPlay);
+    }
+
+    string GetAnimationName() {
+        bool moving = rb2.velocity.magnitude > MOVEMENT_DEADZONE;
+        float x = (rb2.velocity.x < MOVEMENT_DEADZONE && rb2.velocity.x > -MOVEMENT_DEADZONE ? 0 : rb2.velocity.x);
+        float y = (rb2.velocity.y < MOVEMENT_DEADZONE && rb2.velocity.y > -MOVEMENT_DEADZONE ? 0 : rb2.velocity.y);
+
+        if (y > 0 && x == 0) {
+            dir = "n";
+        } else if (x > 0) {
+            if (y > 0)
+                dir = "ne";
+            else if (y == 0)
+                dir = "e";
+            else if (y < 0)
+                dir = "se";
+        } else if (y < 0 && x == 0) {
+            dir = "s";
+        } else if (x < 0) {
+            if (y < 0)
+                dir = "sw";
+            else if (y == 0)
+                dir = "w";
+            else if (y > 0)
+                dir = "nw";
+        }
+        
+        return $"player-{ (moving ? "move" : "idle") }-{ dir }_Clip";
+    }
+}
